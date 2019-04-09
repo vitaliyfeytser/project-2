cf971a678e3633d75ba78d6119f31277f28115cb
VitaliyFeytser <vitaliyfeytser@gmail.com>
2019-04-08 19:24:51 -0700

    main routes are working

--- models/schema.sql
+++ models/schema.sql
@@ -1,7 +1,8 @@
-DROP DATABASE IF EXISTS bookClub;
-CREATE DATABASE bookClub;
+DROP DATABASE IF EXISTS bookClub_db;
+CREATE DATABASE bookClub_db;
 
-use bookClub;
+use bookClub_db;
+
 create table readers
 (
 	id int NOT NULL AUTO_INCREMENT,
@@ -10,43 +11,49 @@ create table readers
 	birthdate date NOT NULL,
     gender varchar(50),
     email varchar(50),
-    current
-	PRIMARY KEY (id),
-    PRIMARY KEY (user_name)
+    createdAt datetime, 
+    updatedAt datetime,
+    -- current
+	PRIMARY KEY (id)
+    -- PRIMARY KEY (user_name)
 );
 
-
-INSERT INTO readers (firstName, lastName, birthdate, gender, email) VALUES ('Kristal', 'Murphy','1985/01/01', 'female', 'iknow@mystuff.com');
-INSERT INTO readers (firstName, lastName, birthdate, gender, email) VALUES ('Tyler', 'Niccols','1979/02/02', 'male', 'awesomecoder@youknowthis.com');
-INSERT INTO readers (firstName, lastName, birthdate, gender, email) VALUES ('Vitaliy', 'Feyster', '1982/03/03', 'male','theukrainetrain@muzhik.com');
-INSERT INTO readers (firstName, lastName, birthdate, gender, email) VALUES ('Alex', 'Tardif', '1979/04/04', 'male', 'myfrontendisbetter@thanyours.com');
-INSERT INTO readers (firstName, lastName, birthdate, gender, email) VALUES ('Patrick', 'Thompson','1980/03/18', 'male', 'pt@alongfortheride.com');
-INSERT INTO readers (firstName, lastName, birthdate, gender, email) VALUES ('Captain', 'Marvel', '1988/06/06', 'female', 'badmamajama@marvel.com');
-INSERT INTO readers (firstName, lastName, birthdate, gender, email) VALUES ('Wonder', 'Wonder', '1984/07/07', 'female','imawonder@dc.com');
-INSERT INTO readers (firstName, lastName, birthdate, gender, email) VALUES ('Billy', 'Gruff', '1960/04/04', 'male', 'thegoat@gruff.com');
-INSERT INTO readers (firstName, lastName, birthdate, gender, email) VALUES ('Joe', 'Bob', '1973/02/02', 'male', 'jb@redneck.com');
-INSERT INTO readers (firstName, lastName, birthdate, gender, email) VALUES ('Super', 'Man', '1965/02/02', 'male', 'imsuper@dc.com');
-INSERT INTO readers (firstName, lastName, birthdate, gender, email) VALUES ('Aunt', 'Sally', '1959/03/03', 'female', 'icanhelp@withalgebra.com');
-INSERT INTO readers (firstName, lastName, birthdate, gender, email) VALUES ('Bille', 'Jean', '1969/04/04', 'female','bj@notmylover.com');
-INSERT INTO readers (firstName, lastName, birthdate, gender, email) VALUES ('Mark', 'Morris', '1970/01/01', 'male','onehit@wonder.com');
-INSERT INTO readers (firstName, lastName, birthdate, gender, email) VALUES ('Leo', 'Qiu', 'leogoesger', '1992/04/15', 'male', 'leo@goesger.com');
-INSERT INTO readers (firstName, lastName, birthdate, gender, email) VALUES ('Mark', 'Davis', '1963/02/02', 'male', 'thehead@raider.com');
+INSERT INTO readers (firstName, lastName, birthdate, gender, email, createdAt, updatedAt) VALUES ('Kristal', 'Murphy','1985/01/01', 'female', 'iknow@mystuff.com', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO readers (firstName, lastName, birthdate, gender, email, createdAt, updatedAt) VALUES ('Tyler', 'Niccols','1979/02/02', 'male', 'awesomecoder@youknowthis.com', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO readers (firstName, lastName, birthdate, gender, email, createdAt, updatedAt) VALUES ('Vitaliy', 'Feyster', '1982/03/03', 'male','theukrainetrain@muzhik.com', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO readers (firstName, lastName, birthdate, gender, email, createdAt, updatedAt) VALUES ('Alex', 'Tardif', '1979/04/04', 'male', 'myfrontendisbetter@thanyours.com', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO readers (firstName, lastName, birthdate, gender, email, createdAt, updatedAt) VALUES ('Patrick', 'Thompson','1980/03/18', 'male', 'pt@alongfortheride.com', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO readers (firstName, lastName, birthdate, gender, email, createdAt, updatedAt) VALUES ('Captain', 'Marvel', '1988/06/06', 'female', 'badmamajama@marvel.com', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO readers (firstName, lastName, birthdate, gender, email, createdAt, updatedAt) VALUES ('Wonder', 'Wonder', '1984/07/07', 'female','imawonder@dc.com', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO readers (firstName, lastName, birthdate, gender, email, createdAt, updatedAt) VALUES ('Billy', 'Gruff', '1960/04/04', 'male', 'thegoat@gruff.com', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO readers (firstName, lastName, birthdate, gender, email, createdAt, updatedAt) VALUES ('Joe', 'Bob', '1973/02/02', 'male', 'jb@redneck.com', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO readers (firstName, lastName, birthdate, gender, email, createdAt, updatedAt) VALUES ('Super', 'Man', '1965/02/02', 'male', 'imsuper@dc.com', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO readers (firstName, lastName, birthdate, gender, email, createdAt, updatedAt) VALUES ('Aunt', 'Sally', '1959/03/03', 'female', 'icanhelp@withalgebra.com', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO readers (firstName, lastName, birthdate, gender, email, createdAt, updatedAt) VALUES ('Bille', 'Jean', '1969/04/04', 'female','bj@notmylover.com', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO readers (firstName, lastName, birthdate, gender, email, createdAt, updatedAt) VALUES ('Mark', 'Morris', '1970/01/01', 'male','onehit@wonder.com', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO readers (firstName, lastName, birthdate, gender, email, createdAt, updatedAt) VALUES ('Leo', 'Qiu', '1992/04/15', 'male', 'leo@goesger.com', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO readers (firstName, lastName, birthdate, gender, email, createdAt, updatedAt) VALUES ('Mark', 'Davis', '1963/02/02', 'male', 'thehead@raider.com', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
 
 create table locations
 (
 	id int NOT NULL AUTO_INCREMENT,
     placeName varchar(50),
-    placeAddress varchar(50,)
+    placeAddress varchar(50),
     meetingTimes varchar(50),
+    createdAt datetime, 
+    updatedAt datetime,
     PRIMARY KEY (id)
-	
-	
 );
 
-INSERT INTO locations (placeName, placeAddress, meetingTimes) VALUES ('Starbucks', '1401 Alhambra Blvd. Sacramento, CA' ,'Friday Night');
-INSERT INTO locations (placeName, placeAddress, meetingTimes) VALUES ('Starbucks', '1020 16th St. Sacramento, CA','Saturday Night');
-INSERT INTO locations (placeName, placeAddress, meetingTimes) VALUES ('Starbucks', '5300 Folsom Blvd. Sacramento, CA','Sunday Night');
-
+INSERT INTO locations (placeName, placeAddress, meetingTimes, createdAt, updatedAt) VALUES ('Starbucks', '1401 Alhambra Blvd. Sacramento, CA' ,'Friday Night', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO locations (placeName, placeAddress, meetingTimes, createdAt, updatedAt) VALUES ('Starbucks', '1401 Alhambra Blvd. Sacramento, CA' ,'Saturday Night', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO locations (placeName, placeAddress, meetingTimes, createdAt, updatedAt) VALUES ('Starbucks', '1401 Alhambra Blvd. Sacramento, CA' ,'Sunday Night', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO locations (placeName, placeAddress, meetingTimes, createdAt, updatedAt) VALUES ('Starbucks', '1020 16th St. Sacramento, CA','Friday Night', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO locations (placeName, placeAddress, meetingTimes, createdAt, updatedAt) VALUES ('Starbucks', '1020 16th St. Sacramento, CA','Saturday Night', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO locations (placeName, placeAddress, meetingTimes, createdAt, updatedAt) VALUES ('Starbucks', '1020 16th St. Sacramento, CA','Sunday Night', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO locations (placeName, placeAddress, meetingTimes, createdAt, updatedAt) VALUES ('Starbucks', '5300 Folsom Blvd. Sacramento, CA','Friday Night', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO locations (placeName, placeAddress, meetingTimes, createdAt, updatedAt) VALUES ('Starbucks', '5300 Folsom Blvd. Sacramento, CA','Saturday Night', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO locations (placeName, placeAddress, meetingTimes, createdAt, updatedAt) VALUES ('Starbucks', '5300 Folsom Blvd. Sacramento, CA','Sunday Night', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
 
 create table promotedBooks
 (
@@ -54,25 +61,71 @@ create table promotedBooks
     title varchar(50),
     author varchar(50),
     monthAndYearPromoted date,
+    createdAt datetime, 
+    updatedAt datetime,
     PRIMARY KEY (id)
-	
-	
 );
 
-INSERT INTO books (title, author, monthAndYearPromoted) VALUES ('Action Bible', 'God', '2019/04/01');
-INSERT INTO books (title, author, monthAndYearPromoted) VALUES ('Kama Sutra for Beginner', 'Joshua Matthew', '2019/04/01');
-INSERT INTO books (title, author, monthAndYearPromoted) VALUES ('Yellow Pages', 'Yellow Pages', '2019/04/01');
-INSERT INTO books (title, author, monthAndYearPromoted) VALUES ('Lord of the Rings', 'J.R.R. Tolkien', '2019/05/01');
-INSERT INTO books (title, author, monthAndYearPromoted) VALUES ('The Wheel of Time', 'Brandon Sanderson', '2019/05/01');
-INSERT INTO books (title, author, monthAndYearPromoted) VALUES ('1984', 'George Orwell', '2019/05/01');
-INSERT INTO books (title, author, monthAndYearPromoted) VALUES ('The Space Trilogy', 'C.S. Lewis', '2019/06/01');
-INSERT INTO books (title, author, monthAndYearPromoted) VALUES ('Enders Game', 'Orson Scott Card', '2019/06/01');
-INSERT INTO books (title, author, monthAndYearPromoted) VALUES ('Brave New World', 'Aldous Huxley', '2019/06/01');
+INSERT INTO promotedBooks (title, author, monthAndYearPromoted, createdAt, updatedAt) VALUES ('Action Bible', 'God', '2019/04/01', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO promotedBooks (title, author, monthAndYearPromoted, createdAt, updatedAt) VALUES ('Kama Sutra for Beginner', 'Joshua Matthew', '2019/04/01', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO promotedBooks (title, author, monthAndYearPromoted, createdAt, updatedAt) VALUES ('Yellow Pages', 'Yellow Pages', '2019/04/01', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO promotedBooks (title, author, monthAndYearPromoted, createdAt, updatedAt) VALUES ('Lord of the Rings', 'J.R.R. Tolkien', '2019/05/01', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO promotedBooks (title, author, monthAndYearPromoted, createdAt, updatedAt) VALUES ('The Wheel of Time', 'Brandon Sanderson', '2019/05/01', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO promotedBooks (title, author, monthAndYearPromoted, createdAt, updatedAt) VALUES ('1984', 'George Orwell', '2019/05/01', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO promotedBooks (title, author, monthAndYearPromoted, createdAt, updatedAt) VALUES ('The Space Trilogy', 'C.S. Lewis', '2019/06/01', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO promotedBooks (title, author, monthAndYearPromoted, createdAt, updatedAt) VALUES ('Enders Game', 'Orson Scott Card', '2019/06/01', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO promotedBooks (title, author, monthAndYearPromoted, createdAt, updatedAt) VALUES ('Brave New World', 'Aldous Huxley', '2019/06/01', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
 
 create table favoriteBooks
 (
+	id int NOT NULL AUTO_INCREMENT,
 	bookId int NOT NULL,
-    userId int NOT NULL
-	
-	
+    readerId int NOT NULL,
+    createdAt datetime, 
+    updatedAt datetime,
+    PRIMARY KEY (id)
 );
+
+INSERT INTO favoriteBooks (bookId, readerId, createdAt, updatedAt) VALUES ('1', '3', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO favoriteBooks (bookId, readerId, createdAt, updatedAt) VALUES ('10', '7', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO favoriteBooks (bookId, readerId, createdAt, updatedAt) VALUES ('10', '4', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO favoriteBooks (bookId, readerId, createdAt, updatedAt) VALUES ('7', '2', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO favoriteBooks (bookId, readerId, createdAt, updatedAt) VALUES ('1', '6', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+
+create table bookClubs
+(
+	id int NOT NULL AUTO_INCREMENT,
+	bookId int NOT NULL,
+    readerId int NOT NULL,
+    locationId int NOT NULL,
+    createdAt datetime, 
+    updatedAt datetime,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO bookClubs (bookId, readerId, locationId, createdAt, updatedAt) VALUES ('1', '3', '3', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO bookClubs (bookId, readerId, locationId, createdAt, updatedAt) VALUES ('1', '2', '3', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO bookClubs (bookId, readerId, locationId, createdAt, updatedAt) VALUES ('1', '1', '3', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO bookClubs (bookId, readerId, locationId, createdAt, updatedAt) VALUES ('1', '6', '3', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO bookClubs (bookId, readerId, locationId, createdAt, updatedAt) VALUES ('10', '7', '2', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO bookClubs (bookId, readerId, locationId, createdAt, updatedAt) VALUES ('10', '6', '2', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO bookClubs (bookId, readerId, locationId, createdAt, updatedAt) VALUES ('10', '3', '2', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO bookClubs (bookId, readerId, locationId, createdAt, updatedAt) VALUES ('7', '8', '3', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO bookClubs (bookId, readerId, locationId, createdAt, updatedAt) VALUES ('3', '10', '3', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO bookClubs (bookId, readerId, locationId, createdAt, updatedAt) VALUES ('3', '11', '3', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO bookClubs (bookId, readerId, locationId, createdAt, updatedAt) VALUES ('3', '15', '3', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO bookClubs (bookId, readerId, locationId, createdAt, updatedAt) VALUES ('3', '9', '3', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO bookClubs (bookId, readerId, locationId, createdAt, updatedAt) VALUES ('3', '12', '3', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO bookClubs (bookId, readerId, locationId, createdAt, updatedAt) VALUES ('3', '4', '3', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+INSERT INTO bookClubs (bookId, readerId, locationId, createdAt, updatedAt) VALUES ('3', '5', '3', '2019-04-06 22:18:59', '2019-04-06 22:18:59');
+
+
+SELECT * FROM locations;
+SELECT * FROM promotedBooks;
+SELECT * FROM favoriteBooks;
+SELECT * FROM bookClubs;
+
+SELECT * FROM readers;
+
+
+-- WRITE SCHEMA FOR JOINING BOOKS WITH USERS
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,84 +1,126 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/bookclubs", function(req, res) {
-    bookClub.readers.findAll({}).then(function(bookClub) {
-      res.json(bookClub);
+  ////////////////////////////////////////////////////////////
+  // GET routes for getting each table's dataset
+  app.get("/api/readers", function(req, res) {
+    db.readers.findAll({}).then(function(results) {
+      res.json(results);
+    });
+    // res.end();
+  });
+
+  app.get("/api/promotedbooks", function(req, res) {
+    db.promotedBooks.findAll({}).then(function(results) {
+      res.json(results);
     });
   });
 
-  app.get("/api/:bookClubID", function(req, res){
-    if(req.params.bookClubID) {
-      bookClub.findOne({
-        where: {
-          bookClubID: req.params.bookClubID
-        }
-      }).then(function(result) {
-        return res.json(result);
-      })
+  app.get("/api/bookclubs", function(req, res) {
+    db.bookClubs.findAll({}).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  app.get("/api/favoritebooks", function(req, res) {
+    db.favoriteBooks.findAll({}).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  app.get("/api/locations", function(req, res) {
+    db.locations.findAll({}).then(function(results) {
+      res.json(results);
+    });
+  });
+  ////////////////////////////////////////////////////////////
+  // GET route for getting ONE item from table's each dataset
+  app.get("/api/bookclubs/:bookClubID", function(req, res) {
+    console.log("---GET request is run here");
+    if (req.params.bookClubID) {
+      console.log("---if statement is run here");
+      db.bookClubs
+        .findOne({
+          where: {
+            id: req.params.bookClubID
+          }
+        })
+        .then(function(results) {
+          return res.json(results);
+        });
     } else {
-      bookClub.findAll().then(function(result){
-        return res.json(result);
-      })
+      console.log("---else is run here");
+      console.log("ERROR: This entry does not exist.");
+
+      db.bookClubs.findAll({}).then(function(all) {
+        // res.json(vit);
+        return res.json(all);
+      });
     }
   });
 
-  app.get("/api/:readerID", function(req,res){
-    if(req.params.readerID){
-      readers.findOne({
-        where: {
-          id: req.params.readerID
-        }
-      }).then(function(result){
-        return res.json(result);
-      })
+  app.get("/api/readers/:readerID", function(req, res) {
+    if (req.params.readerID !== null) {
+      db.readers
+        .findOne({
+          where: {
+            id: req.params.readerID
+          }
+        })
+        .then(function(result) {
+          return res.json(result);
+        });
     } else {
-      // var noresult = {
-      //   id = 'NONE',
-      //   reader_name = "User ID not found",
-      //   age = 0,
-      //   gender = "N/A",
-      //   email = "N/A",
-      // }
+      var noresult = {
+        id: "NONE",
+        readerName: "User ID not found",
+        age: 0,
+        gender: "N/A",
+        email: "N/A"
+      };
       return res.json(noresult);
     }
-  })
-
-  // Create a new example
-  app.post("/api/newreader", function(req, res) {
-    bookClub.readers.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
   });
-//Example of app.post if above code does not work/if we need to define the object more clearly
-//   app.post("/api/new", function(req, res) {
-//     // Take the request...
-//     var character = req.body;
 
-//     // Create a routeName
+  // // Create a new example
+  // app.post("/api/newreader", function (req, res) {
+  //   bookClub.readers.create(req.body).then(function (dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 
-//     // Using a RegEx Pattern to remove spaces from character.name
-//     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-//     var routeName = character.name.replace(/\s+/g, "").toLowerCase();
+  //Example of app.post if above code does not work/if we need to define the object more clearly
+  //   app.post("/api/new", function(req, res) {
+  //     // Take the request...
+  //     var character = req.body;
 
-//     // Then add the character to the database using sequelize
-//     Character.create({
-//       routeName: routeName,
-//       name: character.name,
-//       role: character.role,
-//       age: character.age,
-//       forcePoints: character.forcePoints
-//     });
+  //     // Create a routeName
 
-//     res.status(204).end();
-//   });
-// };
+  //     // Using a RegEx Pattern to remove spaces from character.name
+  //     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  //     var routeName = character.name.replace(/\s+/g, "").toLowerCase();
+
+  //     // Then add the character to the database using sequelize
+  //     Character.create({
+  //       routeName: routeName,
+  //       name: character.name,
+  //       role: character.role,
+  //       age: character.age,
+  //       forcePoints: character.forcePoints
+  //     });
+
+  //     res.status(204).end();
+  //   });
+  // };
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  app.delete("/api/readers/:id", function(req, res) {
+    db.Readers.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbReaders) {
+      res.json(dbReaders);
     });
   });
 };
