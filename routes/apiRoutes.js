d10e49d80aac70134eda14e3e38319046079fcf5
VitaliyFeytser <vitaliyfeytser@gmail.com>
2019-04-10 21:51:15 -0700

    route checks if user exists

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,5 @@
 var db = require("../models");
 
-
 module.exports = function(app) {
   ////////////////////////////////////////////////////////////
   // GET routes for getting each table's dataset
@@ -93,53 +92,56 @@ module.exports = function(app) {
   app.post("/api/reader", function(req, res) {
     var reader = req.body;
     console.log(reader);
-    
-    db.readers.findOne({where: {email: reader.email}}).then(function(user){
-      if(user){
-        res.status(400).send({msg: "user already exist"})
-      } else {
-        db.readers.create({
-          firstName: reader.firstName,
-          lastName: reader.lastName,
-          city: reader.city,
-          stateUS: reader.state,
-          gender: reader.gender,
-          ageRange: reader.age,
-          email: reader.email,
-          bio: reader.bio,
-          password: reader.password
-        }).then(function(newUser){
-          res.staus(200).send(newUser)
-        })
-      }
-    }).catch(function(){
-      db.readers.create({
-        firstName: reader.firstName,
-        lastName: reader.lastName,
-        city: reader.city,
-        stateUS: reader.state,
-        gender: reader.gender,
-        ageRange: reader.age,
-        email: reader.email,
-        bio: reader.bio,
-        password: reader.password
-      }).then(function(newUser){
-        res.staus(200).send(newUser)
+
+    db.readers
+      .findOne({ where: { email: reader.email } })
+      .then(function(user) {
+        if (user) {
+          res.status(400).send({ msg: "user already exist" });
+        } else {
+          db.readers
+            .create({
+              firstName: reader.firstName,
+              lastName: reader.lastName,
+              city: reader.city,
+              stateUS: reader.state,
+              gender: reader.gender,
+              ageRange: reader.age,
+              email: reader.email,
+              bio: reader.bio,
+              password: reader.password
+            })
+            .then(function(newUser) {
+              res.staus(200).send(newUser);
+            });
+        }
       })
-    })
- 
-});
+      .catch(function() {
+        db.readers
+          .create({
+            firstName: reader.firstName,
+            lastName: reader.lastName,
+            city: reader.city,
+            stateUS: reader.state,
+            gender: reader.gender,
+            ageRange: reader.age,
+            email: reader.email,
+            bio: reader.bio,
+            password: reader.password
+          })
+          .then(function(newUser) {
+            res.staus(200).send(newUser);
+          });
+      });
+  });
 
-app.post("/api/favor", function(req, res) {
-  console.log(req.body);
-  
-  db.favoriteBooks.create(
-    req.body
-   );
+  app.post("/api/favor", function(req, res) {
+    console.log(req.body);
 
-  res.status(204).end();
-});
+    db.favoriteBooks.create(req.body);
 
+    res.status(204).end();
+  });
   //Example of app.post if above code does not work/if we need to define the object more clearly
   //   app.post("/api/new", function(req, res) {
   //     // Take the request...
