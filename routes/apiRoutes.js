31e6b24a134fb260e0cb306f155afafb5c153072
VitaliyFeytser <vitaliyfeytser@gmail.com>
2019-04-12 12:45:41 -0700

    commented out a dot-catch in the new user post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -129,30 +129,10 @@ module.exports = function(app) {
     var reader = req.body;
     console.log(reader);
 
-    db.readers
-      .findOne({ where: { email: reader.email } })
-      .then(function(user) {
-        if (user) {
-          res.status(400).send({ msg: "user already exist" });
-        } else {
-          db.readers
-            .create({
-              firstName: reader.firstName,
-              lastName: reader.lastName,
-              city: reader.city,
-              stateUS: reader.state,
-              gender: reader.gender,
-              ageRange: reader.age,
-              email: reader.email,
-              bio: reader.bio,
-              password: reader.password
-            })
-            .then(function(newUser) {
-              res.staus(200).send(newUser);
-            });
-        }
-      })
-      .catch(function() {
+    db.readers.findOne({ where: { email: reader.email } }).then(function(user) {
+      if (user) {
+        res.status(400).send({ msg: "user already exist" });
+      } else {
         db.readers
           .create({
             firstName: reader.firstName,
@@ -168,7 +148,25 @@ module.exports = function(app) {
           .then(function(newUser) {
             res.staus(200).send(newUser);
           });
-      });
+      }
+    });
+    // .catch(function() {
+    //   db.readers
+    //     .create({
+    //       firstName: reader.firstName,
+    //       lastName: reader.lastName,
+    //       city: reader.city,
+    //       stateUS: reader.state,
+    //       gender: reader.gender,
+    //       ageRange: reader.age,
+    //       email: reader.email,
+    //       bio: reader.bio,
+    //       password: reader.password
+    //     })
+    //     .then(function(newUser) {
+    //       res.staus(200).send(newUser);
+    //     });
+    // });
   });
 
   app.post("/api/favor", function(req, res) {
